5b21cf7964e33ec343217d8eca25c18c015fbf9f
n0v4to <den.litvinov.94@list.ru>
2023-11-25 16:13:44 +0400

    def get_image

--- lab1.py
+++ lab1.py
@@ -1,3 +1,17 @@
-from bs4 import BeautifulSoup
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import os
 import requests
-print("sssssss")
+
+def get_image(image_link, name, index):
+    if not os.path.isdir(name):
+        os.mkdir(name)
+    picture = requests.get(image_link)
+    saver = open(os.path.join(f"{name}/{str(index).zfill(4)}.jpg"), "wb")
+    saver.write(picture.content)
+    saver.close()
+
+
+if __name__ == "__main__":
+    main()
