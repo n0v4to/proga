abc207248b4e24f1925c5ff81300f35f63416220
n0v4to <den.litvinov.94@list.ru>
2023-11-28 21:02:47 +0400

    lab2 task2 done

--- task2_lab2.py
+++ task2_lab2.py
@@ -36,8 +36,35 @@ def replace_images(class_name: str) -> list:
 
     os.chdir('..')
 
+def create_dataset2():
+    class_polar_bear="polar bear"
+    class_brown_bear="brown bear"
+    
+    if os.path.isdir('dataset2'):
+        shutil.rmtree('dataset2')
 
-
+    old = os.path.relpath('dataset')
+    new = os.path.relpath('dataset2')
+    shutil.copytree(old, new)
+    
+    replace_images(class_polar_bear)
+    replace_images(class_brown_bear)
+    
+def create_annotation2():
+    class_polar_bear="polar bear"
+    class_brown_bear="brown bear"
+    
+    polar_bear_full_paths = get_full_paths(class_polar_bear)
+    polar_bear_rel_paths = get_relative_paths(class_polar_bear)
+    brown_bear_full_paths = get_full_paths(class_brown_bear)
+    brown_bear_rel_paths = get_relative_paths(class_brown_bear)
+    
+    with open('paths2.csv', 'w') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',', lineterminator='\r')
+        for full_path, rel_path in zip(polar_bear_full_paths, polar_bear_rel_paths):
+            writer.writerow([full_path, rel_path, class_polar_bear])
+        for full_path, rel_path in zip(brown_bear_full_paths, brown_bear_rel_paths):
+            writer.writerow([full_path, rel_path, class_brown_bear])
 
 def main():
     create_dataset2()
