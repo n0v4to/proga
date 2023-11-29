8513f1f75c4eaf485ac33f54ecf5d2594c10fd11
n0v4to <den.litvinov.94@list.ru>
2023-11-29 08:13:23 +0400

    docstring done

--- next_exemplar.py
+++ next_exemplar.py
@@ -1,11 +1,20 @@
 import os
 
 def get_next(class_name) -> str:
+    '''
+    Возвращает список относительных путей ко всем объектам класса
+    Parameters
+    class_name : str
+      Имя класса
+    Returns
+        str
+        Относительный путь к объекту
+    '''
     path = os.path.join('dataset', class_name)
     class_names = os.listdir(path)
     for i in range (len(class_names)):
         if class_names[i] is not None:
-            print(class_names[i])
+            print(os.path.join(path, class_names[i]))
         else:
             print(None)
 
